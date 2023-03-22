0a238e2f639db266ba60c1d2c5760faa3db794ae
Kadhz <carloskadu.silva123@gmail.com>
2023-03-22 16:22:28 -0300

    criação do controller usuarios e paginas index cadastros e usuarios

---
+++ WebApp_tarde/Controllers/UsuariosController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp_tarde.Controllers
+{
+    public class UsuariosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Cadastros()
+        {
+            return View();
+        }
+
+    }
+}
